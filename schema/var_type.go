26959c697979dce141c4dc00072b74d2323bf25d
Vaibhav-Singhal-Alida <vaibhav.singhal@rsystems.com>
2020-11-03 18:27:12 +0530

    Removed schemaType ts as it was added for debugging

--- schema/var_type.go
+++ schema/var_type.go
@@ -123,7 +123,7 @@ func ParseVarTypeExpr(schema *WebRPCSchema, expr string, vt *VarType) error {
 		var value string
 		var err error
 		// parse map expr
-		if schema.SchemaType == "go" || schema.SchemaType == "ts" {
+		if schema.SchemaType == "go" {
 			key, value, err = parseGoSchemaMapExpr(expr)
 			if err != nil {
 				return err
